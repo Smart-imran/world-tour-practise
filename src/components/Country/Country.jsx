e1be34125d4a202c4e5d037fca0b06e7cb6e48ac
Smart-imran <rst.imran4@gmail.com>
2024-11-03 20:48:01 +0600

    proptypes add

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,23 +1,40 @@
+import PropTypes from 'prop-types';
 
+const Country = ({ country }) => {
 
-const Country = ({country}) => {
+    console.log(country);
+    const style = {
+        border: "2px solid red",
+        borderRadius: "10px",
+        padding: "20px",
+        margin: '20px'
+    };
 
-        console.log(country);
-        const style = {
-            border: "2px solid red",
-            borderRadius: "10px",
-            padding: "20px",
-            margin:'20px'
-        };
+    const { name, flags } = country;
 
-        const {name,flags } = country;
+    console.log("Name type:", typeof flags.png);
 
     return (
         <div style={style}>
             <h3>Name : {name?.common}</h3>
-            <img src={flags.png} alt="" />
+            <img src={flags?.png} alt="" />
         </div>
     );
 };
+
+
+Country.propTypes = {
+    country: PropTypes.shape({
+        name : PropTypes.shape({
+            common : PropTypes.string.isRequired,
+        }).isRequired,
+        flags: PropTypes.shape({
+            png: PropTypes.string.isRequired,
+        }).isRequired,
+    })
+};
+
+
+
 
 export default Country;
